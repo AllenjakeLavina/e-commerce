d00953c600e5bf96a5465d7460467e7b1394e45c
AllenjakeLavina <allenjake1010@gmail.com>
2024-12-09 05:38:52 +0800

    cart page

---
+++ src/routes/cart/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    interface CartItem {
+      name: string;
+      price: number;
+    }
+  
+    let cartItems: CartItem[] = []; // Define cartItems as an array of CartItem objects
+  </script>
+  
+  <div class="cart">
+    <h1>Your Cart</h1>
+    {#if cartItems.length === 0}
+      <p>Your cart is empty!</p>
+    {:else}
+      <ul>
+        {#each cartItems as item}
+          <li>{item.name} - ${item.price}</li>
+        {/each}
+      </ul>
+    {/if}
+  </div>
+  
